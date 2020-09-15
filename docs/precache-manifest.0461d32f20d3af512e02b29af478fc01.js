aa9e17a44b386bc40effb42c22ec8a1d0a65e2fe
Jacanroman <jacanroman@gmail.com>
2020-09-15 13:28:13 +0100

    added docs

---
+++ docs/precache-manifest.0461d32f20d3af512e02b29af478fc01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440c0676d7cdeeda71272b589c4c0474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f68e0b0624b11f47a09",
+    "url": "/static/css/main.5014aef5.chunk.css"
+  },
+  {
+    "revision": "5b36e996af0d7ede8a53",
+    "url": "/static/js/2.8a6d1373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a6d1373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f68e0b0624b11f47a09",
+    "url": "/static/js/main.0b8220ee.chunk.js"
+  },
+  {
+    "revision": "7c7e71c3d13998346ba7",
+    "url": "/static/js/runtime-main.8f02d32f.js"
+  }
+]);
